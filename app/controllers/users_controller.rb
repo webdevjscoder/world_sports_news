c1776f28eec71f71a952fc250dfc3e1d6bcb5ce7
webdevjscoder <cobianjuan19@gmail.com>
2020-05-30 09:39:14 -0700

    got homepage to render after logging in

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -6,8 +6,26 @@ class UsersController < ApplicationController
         erb :'users/login'
     end
 
+    # renders signup form
+    get '/signup/new' do
+        erb :'users/signup'
+    end
+
+    # creates a new user
+    post '/signup' do
+        @user = User.create(username: params[:username], email: params[:email], password: params[:password])
+            session[:id] = @user.id
+            redirect to "/teams"
+    end
+
+    # shows user homepage when they login or create and account
+    get '/homepage/:id' do
+        @user = User.find_by(params[:id])
+        erb :"users/homepage"
+    end
+
     # verifies the user's login via their password
-    post '/login' do
+    post '/homepage' do
         @user = User.find_by(params[:id])
         if @user != "" && @user.authenticate(params[:password])
             redirect to "/homepage/#{@user.id}"
@@ -16,37 +34,12 @@ class UsersController < ApplicationController
         end
     end
 
-    # shows user homepage when they login or create and account
-    get '/homepage/:id' do
-        @user = User.find_by(params[:id])
-        erb :"users/homepage/#{@user.id}"
-    end
 
-    # renders signup form
-    get '/signup' do
-        erb :'users/signup'
-    end
-
-    # creates a new user
-    post '/signup' do
-        @user = User.create(username: params[:username], email: params[:email], password: params[:password])
-        session[:id] = @user.id
-        redirect to "/teams"
-    end
 
     get '/logout' do
         session.clear
         erb :'index.html'
     end
-
-    get '/homepage' do
-        erb :'users/homepage'
-    end
     
-    post '/homepage' do
-        binding.pry
-        @user = User.find_by(params[:id])
-        redirect to "/homepage/#{@user.id}"
-    end
 
 end
